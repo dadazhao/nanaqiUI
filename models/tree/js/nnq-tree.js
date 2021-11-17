be9799fdd060653f28bd13f82a267f37082bd167
AcmenZhao <acmenzhao@gmail.com>
2021-11-17 15:40:21 +0800

    rebuild code

--- models/tree/js/nnq-tree.js
+++ models/tree/js/nnq-tree.js
@@ -1,31 +1,40 @@
 window.onload=function(){
-	var treeBranchs = document.getElementsByClassName("nnq-tree__branch");
+	var branchClassName = "nnq-tree__branch";
+	var hideClassName = "nnq-hide";
+	
+	var outlineTreeOpenClassName = "nnq-icon-minus-circle";
+	var outlineTreeCloseClassName = "nnq-icon-plus-circle";
+	
+	var defaultTreeOpenClassName = "nnq-icon-down-triangle";
+	var defaultTreeCloseClassName = "nnq-icon-right-triangle";
+	
+	var treeBranchs = document.getElementsByClassName(branchClassName);
 	for(var i=0;i<treeBranchs.length;i++){
 		treeBranchs[i].onclick=function(){
-			if(this.className.indexOf("nnq-icon-minus-circle")!=-1){
-				this.className = this.className.replace("nnq-icon-minus-circle","nnq-icon-plus-circle")
+			if(this.className.indexOf(outlineTreeOpenClassName)!=-1){
+				this.className = this.className.replace(outlineTreeOpenClassName,outlineTreeCloseClassName)
 				if(this.parentNode.nextElementSibling){
-					this.parentNode.nextElementSibling.className = "nnq-hide";
+					this.parentNode.nextElementSibling.className = hideClassName;
 				}
 				return;
 			}
-			if(this.className.indexOf("nnq-icon-plus-circle")!=-1){
-				this.className = this.className.replace("nnq-icon-plus-circle","nnq-icon-minus-circle")
+			if(this.className.indexOf(outlineTreeCloseClassName)!=-1){
+				this.className = this.className.replace(outlineTreeCloseClassName,outlineTreeOpenClassName)
 				if(this.parentNode.nextElementSibling){
 					this.parentNode.nextElementSibling.className = "";
 				}
 				return;
 			}
 			
-			if(this.className.indexOf("nnq-icon-down-triangle")!=-1){
-				this.className = this.className.replace("nnq-icon-down-triangle","nnq-icon-right-triangle")
+			if(this.className.indexOf(defaultTreeOpenClassName)!=-1){
+				this.className = this.className.replace(defaultTreeOpenClassName,defaultTreeCloseClassName)
 				if(this.parentNode.nextElementSibling){
-					this.parentNode.nextElementSibling.className = "nnq-hide";
+					this.parentNode.nextElementSibling.className = hideClassName;
 				}
 				return;
 			}
-			if(this.className.indexOf("nnq-icon-right-triangle")!=-1){
-				this.className = this.className.replace("nnq-icon-right-triangle","nnq-icon-down-triangle")
+			if(this.className.indexOf(defaultTreeCloseClassName)!=-1){
+				this.className = this.className.replace(defaultTreeCloseClassName,defaultTreeOpenClassName)
 				if(this.parentNode.nextElementSibling){
 					this.parentNode.nextElementSibling.className = "";
 				}
