cdf3b7ff03c6f73e8798dce1186757f9f73b34e1
zylong-hsbc <acmenzhao@gmail.com>
2021-12-07 18:18:54 +0800

    add nnq object

--- models/alter/js/nnq-alter.js
+++ models/alter/js/nnq-alter.js
@@ -1,36 +1,56 @@
 var nnq = function(selector) {
 	return new nnq.fn.init(selector)
 }
+
 nnq.fn = nnq.prototype = {
 	init: function(selector) {
 		this[0] = selector;
 		return this
 	}
 }
+// object extend method
+nnq.extend=function(destination,source){
+    if(typeof destination == "object"){
+        if(typeof source == "object"){
+            for(var i in source){
+                destination[i] = source[i];
+            }
+        }
+    }
+    if(typeof destination == "function"){
+        if(typeof source == "object"){
+            for(var i in source){
+                destination.prototype[i] = source[i];
+            }
+        }
+        if(typeof source == "function"){
+            destination.prototype = source.prototype;
+        }
+    }
+    return destination;
+}
+nnq.data = nnq.prototype.data = function(name,value){
+	
+}
 nnq.fn.init.prototype = nnq.fn;
 window.nnq = nnq;
 
-nnq.fn.alter = function() {
-	var message = {
-		"datetime": "",
-		"message": ""
+
+
+(function(nnq){
+	
+	var alterList=[];
+	
+	function init(nnq){
+		initalizeElement(nnq);
 	}
-	var html = document.getElementsByTagName("BODY")[0];
-	var alter_ele = [];
-	var alter_style = 'style="top:86px;z-index:2500;"';
-	var alter_text = '信息提示';
-	alter_ele.push(' <div class="nnq-message nnq-message--primary" ');
-	alter_ele.push(alter_style);
-	alter_ele.push('>');
-	alter_ele.push('<i class="nnq-message-icon nnq-icon-warnning"></i>');
-	alter_ele.push('<span class="nnq-message-lable">');
-	alter_ele.push(alter_text);
-	alter_ele.push('</span>');
-	alter_ele.push('</div>');
-	html.innerHTML = html.innerHTML + alter_ele.join('');
-};
-
-
-window.onload = function() {
-	nnq.fn.alter();
-}
+	
+	function initalizeElement(nnq){
+		var alter_ele = [];
+		alter_ele.push(' <div class="nnq-message nnq-message--primary" >');
+		alter_ele.push('<i class="nnq-message-icon nnq-icon-warnning"></i>');
+		alter_ele.push('<span class="nnq-message-lable">');
+		alter_ele.push('</span>');
+		alter_ele.push('</div>');
+	}
+})(nnq)
