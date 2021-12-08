2d89330770206d3dd6a24d0565e25932b90f713e
zylong-hsbc <acmenzhao@gmail.com>
2021-12-08 18:07:30 +0800

    add nna data extend

--- models/alter/js/nnq-alter.js
+++ models/alter/js/nnq-alter.js
@@ -9,43 +9,70 @@ nnq.fn = nnq.prototype = {
 	}
 }
 // object extend method
-nnq.extend=function(destination,source){
-    if(typeof destination == "object"){
-        if(typeof source == "object"){
-            for(var i in source){
-                destination[i] = source[i];
-            }
-        }
-    }
-    if(typeof destination == "function"){
-        if(typeof source == "object"){
-            for(var i in source){
-                destination.prototype[i] = source[i];
-            }
-        }
-        if(typeof source == "function"){
-            destination.prototype = source.prototype;
-        }
-    }
-    return destination;
+nnq.extend = function(destination, source) {
+	if (typeof destination == "object") {
+		if (typeof source == "object") {
+			for (var i in source) {
+				destination[i] = source[i];
+			}
+		}
+	}
+	if (typeof destination == "function") {
+		if (typeof source == "object") {
+			for (var i in source) {
+				destination.prototype[i] = source[i];
+			}
+		}
+		if (typeof source == "function") {
+			destination.prototype = source.prototype;
+		}
+	}
+	return destination;
 }
-nnq.data = nnq.prototype.data = function(name,value){
-	
+
+var expando = "NNQ" + ("1.6" + Math.random()).replace(/\D/g, '');  
+
+nnq.data = function(obj, name, value) {
+
+function getData(cache, name) {
+	return cache[name];
+}
+
+function setData(cache, name, value) {
+	cache[name] = value;
+}
+
+function getCache(obj) {
+	obj[expando] = obj[expando] || {};
+	return obj[expando];
+}
+	var cache = getCache(obj);
+
+	if (value === undefined) {
+		return getData(cache, name);
+	} else {
+		setData(cache, name, value);
+	}
 }
 nnq.fn.init.prototype = nnq.fn;
 window.nnq = nnq;
 
 
+nnq.data(nnq, "aaa", {"bbb":"bbb"});
+console.log("get==" + nnq.data(nnq, "aaa"));
 
-(function(nnq){
-	
-	var alterList=[];
-	
-	function init(nnq){
+console.log(nnq);
+
+console.log(nnq("a"));
+(function(nnq) {
+
+	var alterList = [];
+
+	function init(nnq) {
 		initalizeElement(nnq);
 	}
-	
-	function initalizeElement(nnq){
+
+	function initalizeElement(nnq) {
 		var alter_ele = [];
 		alter_ele.push(' <div class="nnq-message nnq-message--primary" >');
 		alter_ele.push('<i class="nnq-message-icon nnq-icon-warnning"></i>');
@@ -53,4 +80,5 @@ window.nnq = nnq;
 		alter_ele.push('</span>');
 		alter_ele.push('</div>');
 	}
+
 })(nnq)
