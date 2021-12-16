f80ad7959b88101f39e99c06d1f57ff2e6fa4a54
zylong-hsbc <acmenzhao@gmail.com>
2021-12-16 16:01:58 +0800

    add table body

--- js/extends/datagrid.js
+++ js/extends/datagrid.js
@@ -10,7 +10,7 @@ nnq(function() {
         //create table header
         createTableHeader(jq);
         //create table body
-        //createTableBody(1, jq);
+        createTableBody(1, jq);
         //create table footer
         //createTableFooter(jq);
     }
@@ -51,11 +51,11 @@ nnq(function() {
         //table column format
         var cols = opts.columns;
         //header start
-        var ele = ["<tr>"];
+        var ele = ['<tr>'];
         for (var i = 0; i < cols.length; i++) {
             if (cols[i].field == 'ck') {
                 if (cols[i].width == null) {
-                    ele.push("<th width='5%'>");
+                    ele.push('<th width="5%">');
                 } else {
                     ele.push('<th width="');
                     ele.push(cols[i].width);
@@ -76,10 +76,78 @@ nnq(function() {
         }
         ele.push('</tr>');
         //add to header element
-        nnq(opts.id).find(".nnq-table-header").each(function() {
+        nnq(opts.id).find('.nnq-table-header').each(function() {
             nnq(this).html(ele.join(""));
         })
 
+    }
+
+    function createTableBody(pn, jq) {
+        var datagrid = nnq.data(jq, "datagrid");
+        var opts = datagrid.options;
+        //table column format
+        var cols = opts.columns;
+        if (opts.url != null) {
+            opts.data = getAjaxData(opts.url, opts.method, opts.params);
+        }
+        //data json 
+        var json = opts.data[opts.dataName];
+        var totalNum = opts.data[opts.totalName];
+        //is use paging
+        if (opts.paging) {
+            //calculation total page num
+            opts.totalPage = Math.ceil(totalNum / opts.pageSize);
+            //start row num
+            var startRow = opts.pageSize * (pn - 1);
+            //end rows num
+            var endRow = startRow + opts.pageSize;
+        } else {
+            //calculation total page num
+            opts.totalPage = Math.ceil(json.length / opts.pageSize);
+            //start row num
+            var startRow = 0;
+            //end rows num
+            var endRow = json.length;
+        }
+
+        //data element
+        var ele = [];
+
+        for (var i = startRow; i < endRow; i++) {
+            //Crossing the line detection
+            if (i >= json.length) break;
+            ele.push('<tr>');
+            //list cols
+            for (var j = 0; j < cols.length; j++) {
+                //user interface
+                if (cols[j].template != null) {
+                    ele.push('<td width=' + cols[j].width + ' align=' + cols[j].align + '>');
+                    ele.push('<div class="nnq-cell">');
+                    ele.push(cols[j].template(json[i]));
+                    ele.push('</div>');
+                    ele.push('</td>');
+                } else {
+                    if (cols[j].field == 'ck') {
+                        if (cols[j].width == null) {
+                            ele.push('<td width="5%" align="center">');
+                        } else {
+                            ele.push('<td width="' + cols[j].width + '" align="center">');
+                        }
+                        ele.push('<label class="nnq-checkbox"> <span class="nnq-checkbox--input"> <input type="checkbox" hidden="hidden"> <span class="nnq-checkbox--icon"></span> </span> <span class="nnq-checkbox--lable"></span> </label></td>');
+                    } else {
+                        ele.push('<td width=' + cols[j].width + ' align=' + cols[j].align + '>');
+                        ele.push('<div class="nnq-cell">');
+                        ele.push(json[i][cols[j].field]);
+                        ele.push('</div>');
+                        ele.push('</td>');
+                    }
+                }
+            }
+            ele.push('</tr>');
+        }
+        nnq(opts.id).find('.nnq-table-body').each(function() {
+            nnq(this).html(ele.join(""));
+        })
     }
 
     nnq.fn.datagrid = function(options, param) {
@@ -113,6 +181,20 @@ nnq(function() {
         });
     }
 
+    function getAjaxData(url, type, params) {
+        var result;
+        nnq.ajax({
+            url: url,
+            type: type,
+            async: false,
+            //async
+            data: params,
+            success: function(data) {
+                result = data;
+            }
+        });
+        return JSON.parse(result);;
+    }
     nnq.fn.datagrid.defaults = {
         // element id ps:"#id"
         id: '',
